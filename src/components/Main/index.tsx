7f4c00fafa90f07f658129f7ef847ada29f87b54
jefferson.souza <jefferson.souza@synchro.com.br>
2021-01-13 13:44:39 -0300

    configuring storybooks

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+const Main = ({ title = 'Advanced React', description = 'Typescript, ReactJS, NextJS and Styled Components' }) => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Logo with text Advanced React, in Portuguese" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="A developer sit in front of the computer" />
+  </S.Wrapper>
+);
+
+export default Main;
